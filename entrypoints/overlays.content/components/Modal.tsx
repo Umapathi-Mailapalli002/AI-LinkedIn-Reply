395e74dd37dd619d910f9ce9ad9d8babc97f1bdb
Umapathi-Mailapalli002 <ravimailapalli62@gmail.com>
2024-09-28 17:08:20 +0530

    update the regenerate button and insert button

--- entrypoints/overlays.content/components/Modal.tsx
+++ entrypoints/overlays.content/components/Modal.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import { GenerateIcon } from './Icons';
+import React, { useState, useEffect } from 'react';
+import { GenerateIcon, REGenerateIcon, InserIcon } from './Icons';
 
 interface ModalProps {
   isOpen: boolean;
@@ -7,26 +7,49 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState<string>("")
+  const [isReqSend, setIsReqSend] = useState<boolean>(false);
+  const [isres, setIsRes] = useState<boolean>(false);
+  const [req, setReq] = useState<string>("")
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!isOpen) {
+      // Reset states when the modal closes
+      setInput("");
+      setIsReqSend(false);
+      setIsRes(false);
+      setReq("");
+      setLoading(false);
+    }
+  }, [isOpen]);
+
 
   if (!isOpen) return null;
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  }
   const handleGenerateRes = () => {
-    
-  }
-const fetchData = async() => {
-  try {
-    const res = await fetch("public\data\response.json");
-    if (!res.ok) {
-      throw new Error(`HTTP error! Status: ${res.status}`);
+    if (input.trim() !== "") {
+      setIsReqSend(true);
+    setReq(input);
+    setInput("");
+    setLoading(true);
+    setTimeout(() => {
+      setIsRes(true)
+      setLoading(false);
+    }, 3000);
     }
-    const result = await res.json();
-    console.log("response of data",result)
-  } catch (error) {
-    console.log("error on fetching data", error)
   }
-}
-fetchData();
+
+  const handleInsert = () => {
+
+  }
+
+  
+  const response: String = "Thank you for the opportunity! If you have any more questions or if there's anything else I can help you with, feel free to ask.";
+
   return (
     <div
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30"
@@ -36,20 +59,29 @@ fetchData();
         className="bg-white p-8 rounded-2xl shadow-lg shadow-[#575757a7] lg:w-[29vw] w-[50vw] sm:ml-[30rem] lg:-ml-20"
         onClick={(e) => e.stopPropagation()}
       >
-        <div className='flex justify-end mb-5 '>
-        <p className='bg-[#DFE1E7] p-4 rounded-xl text-gray-500 w-8/12 text-[1.37rem]'>{input}</p>
-      </div>
-        <div className='flex justify-start mb-5 '>
-        <p className='bg-[#DBEAFE] p-4 rounded-xl text-gray-500 w-10/12 text-[1.37rem]'>Thank you for the opportunity! If you have any more questions or if there's anything else I can help you with, feel free to ask.</p>
-      </div>
+
+        {isReqSend && (<div className='flex justify-end mb-5 '>
+          <p className='bg-[#DFE1E7] p-4 rounded-xl text-gray-500 w-8/12 text-[1.37rem]'>{req}</p>
+        </div>)}
+        {loading && (<div className='flex justify-start mb-5 '>
+          <Loading />
+        </div>)}
+        {isres && (<div className='flex justify-start mb-5 '>
+          <p className='bg-[#DBEAFE] p-4 rounded-xl text-gray-500 w-10/12 text-[1.37rem]'>{response}</p>
+        </div>)}
+
         <div className=''>
-          <input  className='rounded-md hover:outline-0 shadow-transparent' placeholder='Your prompt' type="text" />
+          <input value={input} className='rounded-md hover:outline-0 shadow-transparent' onChange={handleInputChange} placeholder='Your prompt' type="text" />
         </div>
 
         <div className='flex justify-end mt-6'>
-          <button onClick={handleGenerateRes} className="">
+        {!isReqSend ? (<button onClick={handleGenerateRes} className="">
             <GenerateIcon />
-          </button>
+          </button>) :
+          (<div className='flex'>
+            <button onClick={handleInsert} className='mr-4'><InserIcon /></button>
+            <button className='cursor-not-allowed'><REGenerateIcon /></button>
+          </div>)}
         </div>
 
       </div>
@@ -57,4 +89,23 @@ fetchData();
   );
 };
 
+
+const Loading: React.FC = () => {
+  return (
+    <>
+      <span className="relative flex h-3 w-3 mr-2">
+        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+        <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+      </span>
+      <span className="relative flex h-3 w-3 mr-2">
+        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+        <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+      </span>
+      <span className="relative flex h-3 w-3 mr-2">
+        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+        <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+      </span>
+    </>
+  )
+}
 export default Modal;
